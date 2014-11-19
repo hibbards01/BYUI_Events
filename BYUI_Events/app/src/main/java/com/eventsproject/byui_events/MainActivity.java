b838956074bb6bece88baa919227d140d74e309d
Samuel Hibbard <hibbards01@gmail.com>
2014-11-19 16:21:56 -0700

    Finished everything!

--- BYUI_Events/app/src/main/java/com/eventsproject/byui_events/MainActivity.java
+++ BYUI_Events/app/src/main/java/com/eventsproject/byui_events/MainActivity.java
@@ -37,43 +37,43 @@ public class MainActivity extends TabActivity {
         Thread thread = new Thread(new SQLDatabase());
         thread.start();
         //create tabs!
-//        TabHost tabHost = (TabHost)findViewById(android.R.id.tabhost);
-//
-//        TabHost.TabSpec tab1 = tabHost.newTabSpec("DayTab");
-//        TabHost.TabSpec tab2 = tabHost.newTabSpec("WeekTab");
-//        TabHost.TabSpec tab3 = tabHost.newTabSpec("MonthTab");
-//        TabHost.TabSpec tab4 = tabHost.newTabSpec("MyEventsTab");
-//
-//        //set up tab name and activity
-//        tab1.setIndicator("Day");
-//        tab1.setContent(new Intent(this, DayActivity.class));
-//
-//        tab2.setIndicator("Week");
-//        tab2.setContent(new Intent(this, WeekActivity.class));
-//
-//        tab3.setIndicator("Month");
-//        tab3.setContent(new Intent(this, MonthActivity.class));
-//
-//        tab4.setIndicator("My Events");
-//        tab4.setContent(new Intent(this, MyEventsActivity.class));
-//
-//        //now add to the host!
-//        tabHost.addTab(tab1);
-//        tabHost.addTab(tab2);
-//        tabHost.addTab(tab3);
-//        tabHost.addTab(tab4);
-//
-//        //now change the indicator color!
-//        for(int i = 0; i < tabHost.getTabWidget().getChildCount(); i++) {
-//            TextView textView = (TextView)tabHost.getTabWidget().getChildAt(i).findViewById(android.R.id.title);
-//            textView.setTextColor(getResources().getColor(R.color.white));
-//            textView.setTextSize(15);
-//        }
-//
-//        tabHost.setCurrentTab(1);
-//
-//        //call the constructor for the database!
-//        Log.d("SQL", "HERE!");
+        TabHost tabHost = (TabHost)findViewById(android.R.id.tabhost);
+
+        TabHost.TabSpec tab1 = tabHost.newTabSpec("DayTab");
+        TabHost.TabSpec tab2 = tabHost.newTabSpec("WeekTab");
+        TabHost.TabSpec tab3 = tabHost.newTabSpec("MonthTab");
+        TabHost.TabSpec tab4 = tabHost.newTabSpec("MyEventsTab");
+
+        //set up tab name and activity
+        tab1.setIndicator("Day");
+        tab1.setContent(new Intent(this, DayActivity.class));
+
+        tab2.setIndicator("Week");
+        tab2.setContent(new Intent(this, WeekActivity.class));
+
+        tab3.setIndicator("Month");
+        tab3.setContent(new Intent(this, MonthActivity.class));
+
+        tab4.setIndicator("My Events");
+        tab4.setContent(new Intent(this, MyEventsActivity.class));
+
+        //now add to the host!
+        tabHost.addTab(tab1);
+        tabHost.addTab(tab2);
+        tabHost.addTab(tab3);
+        tabHost.addTab(tab4);
+
+        //now change the indicator color!
+        for(int i = 0; i < tabHost.getTabWidget().getChildCount(); i++) {
+            TextView textView = (TextView)tabHost.getTabWidget().getChildAt(i).findViewById(android.R.id.title);
+            textView.setTextColor(getResources().getColor(R.color.white));
+            textView.setTextSize(15);
+        }
+
+        tabHost.setCurrentTab(1);
+
+        //call the constructor for the database!
+        Log.d("SQL", "HERE!");
 
         /*if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
@@ -93,30 +93,30 @@ public class MainActivity extends TabActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
 
-//        this.menu = menu;
-//
-//        MenuItem searchBar = menu.findItem(R.id.action_search);
-//        SearchView searchView = (SearchView)searchBar.getActionView();
-//        searchView.setQueryHint("Search");
-//
-//        //now for the textview!
-//
-//        int searchPlateId = searchView.getContext().getResources().getIdentifier("android:id/search_plate", null, null);
-//        View searchPlate = searchView.findViewById(searchPlateId);
-//
-//
-//        if (searchPlate!=null) {
-//            searchPlate.setBackgroundColor(getResources().getColor(R.color.darkgray));
-//
-//            int searchTextId = searchPlate.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
-//            TextView searchText = (TextView) searchPlate.findViewById(searchTextId);
-//
-//            if (searchText!=null) {
-//                searchText.setTextColor(getResources().getColor(R.color.white));
-//                searchText.setHintTextColor(getResources().getColor(R.color.white));
-//                searchText.setTextSize(15);
-//            }
-//        }
+        this.menu = menu;
+
+        MenuItem searchBar = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView)searchBar.getActionView();
+        searchView.setQueryHint("Search");
+
+        //now for the textview!
+
+        int searchPlateId = searchView.getContext().getResources().getIdentifier("android:id/search_plate", null, null);
+        View searchPlate = searchView.findViewById(searchPlateId);
+
+
+        if (searchPlate!=null) {
+            searchPlate.setBackgroundColor(getResources().getColor(R.color.darkgray));
+
+            int searchTextId = searchPlate.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
+            TextView searchText = (TextView) searchPlate.findViewById(searchTextId);
+
+            if (searchText!=null) {
+                searchText.setTextColor(getResources().getColor(R.color.white));
+                searchText.setHintTextColor(getResources().getColor(R.color.white));
+                searchText.setTextSize(15);
+            }
+        }
 
         return true;
     }
@@ -135,45 +135,45 @@ public class MainActivity extends TabActivity {
         int id = item.getItemId();
         Log.d("HERE!", "HERE!");
 
-//        switch(id) {
-//            case R.id.action_settings:
-//                Toast.makeText(this, "Running settings", Toast.LENGTH_LONG).show();
-//                return true;
-//            case R.id.action_search:
-//                Toast.makeText(this, "Running search", Toast.LENGTH_LONG).show();
-//                return true;
-//            case R.id.filter_activities_complete:
-//            case R.id.filter_activities_life_skills:
-//            case R.id.filter_activities_social:
-//            case R.id.filter_activities_sports:
-//            case R.id.filter_activities_talent:
-//            case R.id.filter_activities_wellness:
-//            case R.id.filter_alumni_or_reunion:
-//            case R.id.filter_broadcast_conference:
-//            case R.id.filter_concert:
-//            case R.id.filter_conference_workshop:
-//            case R.id.filter_devo_speeches:
-//            case R.id.filter_get_connected:
-//            case R.id.filter_graduation:
-//            case R.id.filter_performance_events:
-//            case R.id.filter_performing_visual_arts:
-//            case R.id.filter_reception_openhouse:
-//            case R.id.filter_reception_public:
-//            case R.id.filter_theatre: {
-//                if (item.isChecked()) {
-//                    item.setChecked(false);
-//                } else {
-//                    item.setChecked(true);
-//                }
-//                return true;
-//            }
-//            case R.id.action_clear_filters: {
-//                unCheckBoxes();
-//                return true;
-//            }
-//            default:
+        switch(id) {
+            case R.id.action_settings:
+                Toast.makeText(this, "Running settings", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.action_search:
+                Toast.makeText(this, "Running search", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.filter_activities_complete:
+            case R.id.filter_activities_life_skills:
+            case R.id.filter_activities_social:
+            case R.id.filter_activities_sports:
+            case R.id.filter_activities_talent:
+            case R.id.filter_activities_wellness:
+            case R.id.filter_alumni_or_reunion:
+            case R.id.filter_broadcast_conference:
+            case R.id.filter_concert:
+            case R.id.filter_conference_workshop:
+            case R.id.filter_devo_speeches:
+            case R.id.filter_get_connected:
+            case R.id.filter_graduation:
+            case R.id.filter_performance_events:
+            case R.id.filter_performing_visual_arts:
+            case R.id.filter_reception_openhouse:
+            case R.id.filter_reception_public:
+            case R.id.filter_theatre: {
+                if (item.isChecked()) {
+                    item.setChecked(false);
+                } else {
+                    item.setChecked(true);
+                }
+                return true;
+            }
+            case R.id.action_clear_filters: {
+                unCheckBoxes();
+                return true;
+            }
+            default:
                 return super.onOptionsItemSelected(item);
-        //}
+        }
     }
 
     /**
---
+++ BYUI_Events/app/src/main/java/com/eventsproject/byui_events/MonthActivity.java
@@ -0,0 +1,32 @@
+package com.eventsproject.byui_events;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MonthActivity extends ListActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String [] list = new String[] {
+                "Linux Meeting", "Career Fair", "I-Connect", "Game Night",
+                "Theater", "Stem Fair", "Basketball", "Baseball", "Softball",
+                "Ping Pong", "Racket ball", "Car Show", "English", "Math",
+                "Red Hat", "Jumping Ropes"
+        };
+
+        //now to put it on the screen!
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.activity_month,
+                R.id.label, list);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView list, View v, int position, long id) {
+
+    }
+}
